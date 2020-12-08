cfaf02122a79fe4b26bb4b640ba37413eec2bedc
MitchRogers <mitchellmel@outlook.com>
2020-12-08 13:19:44 -0700

    models and migrations

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class listing(models.Model) :
+    listing_id = models.IntegerField(primary_key=True)
+    status = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100)
+    organization_id = models.IntegerField
+    contracts = models.CharField(max_length=50)
+    description = models.CharField(max_length=9000)
+
+    def __str__(self) :
+        return (self.job_title + ' - ' + self.city)
+
+class Skill(models.Model) :
+    skill_id = models.IntegerField(primary_key=True)
+    skill_name = models.CharField(max_length=200)
+
+    def __str__(self) :
+        return (self.skill_name)
--- organization/models.py
+++ organization/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from applicant.models import Applicant
 
 # Create your models here.
+class Organization(models.Model) :
+    organization_id = models.IntegerField(primary_key=True)
+    company_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+    address = models.CharField(max_length=100)
+    size = models.CharField(max_length=20)
+    sectors = models.CharField(max_length=50)
+
+    def __str__(self) :
+        return (self.company_name)
+
+class Offer(models.Model) :
+    offer_id = models.IntegerField(primary_key=True)
+    applicant_id = models.ForeignKey("applicant.Applicant", on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=20)
+    job_title = models.CharField(max_length=100)
+    organization_id = models.ForeignKey("Organization", on_delete=models.DO_NOTHING)
+    contracts = models.CharField(max_length=50)
+    matching_skills = models.IntegerField
+
+    def __str__(self) :
+        return (self.offer_id + ' - ' + self.job_title)
