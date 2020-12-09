2eaf25a373602677d4964bf625b5ab655c8b97ef
MitchRogers <mitchellmel@outlook.com>
2020-12-09 15:09:38 -0700

    started listings page

--- listings/views.py
+++ listings/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def indexPageView(request) :
-    return render(request, 'listings/allListings.html')
+    context = {
+        'list' : [1,2,3,4,5]
+    }
+    return render(request, 'listings/allListings.html', context)
 
 def displayListingPageView(request) :
     return HttpResponse('Display Listing View')
