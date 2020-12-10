2c08858d4a69f39d6385b685d8a1366ef56169ed
MitchRogers <mitchellmel@outlook.com>
2020-12-09 18:24:03 -0700

    update applicant crud

--- applicant/urls.py
+++ applicant/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import indexPageView, deleteApplicantPageView, updateApplicantsPageView, displayApplicantPageView, removeApplicantPageView, addApplicantPageView, registerPageView
+from .views import indexPageView, deleteApplicantPageView, alterApplicantPageView, updateApplicantsPageView, displayApplicantPageView, removeApplicantPageView, addApplicantPageView, registerPageView
 
 urlpatterns = [
-    path('searchApplicants/', updateApplicantsPageView, name='update_applicant'),
+    path('alterApplicant/', alterApplicantPageView, name='alter_applicant'),
+    path('updateApplicants/', updateApplicantsPageView, name='update_applicant'),
     path('removeApplicant/', removeApplicantPageView, name='remove_applicant'),
     path('registerApplicant/', registerPageView, name='register_applicant'),
     path('deleteApplicant/', deleteApplicantPageView, name='delete_applicant'),
--- applicant/views.py
+++ applicant/views.py
@@ -17,6 +17,24 @@ def deleteApplicantPageView(request) :
 def updateApplicantsPageView(request) :
     return render(request, 'applicant/updateapplicant.html')
 
+def alterApplicantPageView(request) :
+    if request.method == 'POST':
+        firstName = request.POST.get('first_name')
+        lastName = request.POST.get('last_name')
+        username = request.POST.get('username')
+        new_firstName = request.POST.get('new_first_name')
+        new_lastName = request.POST.get('new_last_name')
+        new_username = request.POST.get('new_username')
+
+        applicant = Applicant.objects.get(first_name=firstName, last_name=lastName, username=username)
+        applicant.first_name = new_firstName
+        applicant.last_name = new_lastName
+        applicant.username = new_username
+
+        applicant.save()
+        
+        return HttpResponseRedirect("/applicant/")
+
 def removeApplicantPageView(request) :
     applicant = Applicant.objects.get(first_name=request.POST['first_name'], last_name=request.POST['last_name'], username=request.POST['username'])
     applicant.delete()
